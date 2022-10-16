4fdf05405cb545ba6b40239772e816b8aa49152d
haller33 <eliseu42lucena@gmail.com>
2022-10-16 00:41:21 -0300

    new little changes

--- src/tester.c
+++ src/tester.c
@@ -1,14 +1,29 @@
 #include <stdio.h>
-#include "orgmodeparser.h"
+#include <string.h>
+
+#include "lib/orgmodeparser.h"
 
 #define ORGMODEPARSERC_H_
 
 int
-main () {
+main (int argc, char *argv[]) {
 
+  if (argc < 2) {
+    puts("no file specify");
+    return 127;
+  }
 
+  FILE *fp = fopen(argv[1], "ro");
+  
+  if (fp == NULL) {
+    printf ("file not open correcly :: %s", argv[1]);
+    return 1;
+  }
+  
   printf("Hello World");
   
-  return 0;
 
+
+  fclose ( fp );
+  return 0;
 }
